d883a9572039f22bde4f8cecab3213b0f90c0d73
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-20 00:42:17 +0700

    If statement

---
+++ tests/if.test.ts
@@ -0,0 +1,21 @@
+describe("If Statement", () => {
+  it("should support in typescript", () => {
+    const examValue = 90;
+
+    if (examValue > 80) {
+      console.info("Good");
+    } else if (examValue > 60) {
+      console.info("Not Bad");
+    } else {
+      console.info("Try Again");
+    }
+  });
+
+  it("should support ternary operator", () => {
+    const value = 80;
+
+    const say = value >= 75 ? "Congratulation" : "Try Again";
+
+    console.info(say);
+  });
+});
