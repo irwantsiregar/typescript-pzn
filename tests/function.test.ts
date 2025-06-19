ada44ac9d322eb0d1d516f9630059fd658535bb6
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-19 22:39:20 +0700

    function parameters

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -21,4 +21,31 @@ describe("Function", () => {
     expect(sayHello()).toBe("Hello Guest");
     expect(sayHello("Irwan")).toBe("Hello Irwan");
   });
+
+  it("should support rest parameter", () => {
+    function sum(...values: number[]): number {
+      let total = 0;
+
+      for (const value of values) {
+        total += value;
+      }
+
+      return total;
+    }
+
+    expect(sum(1, 2, 3, 4, 5)).toBe(15);
+  });
+
+  it("should support optional parameter", () => {
+    function sayaHello(firstName: string, lastName?: string): string {
+      if (lastName) {
+        return `Hello ${firstName} ${lastName}`;
+      } else {
+        return `Hello ${firstName}`;
+      }
+    }
+
+    expect(sayaHello("Irwan")).toBe("Hello Irwan");
+    expect(sayaHello("Irwan Siregar")).toBe("Hello Irwan Siregar");
+  });
 });
