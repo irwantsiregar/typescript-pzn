1b61fd4c8ad37c871748821ee68110df72c282a2
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-19 16:17:14 +0700

    data type: interface with readonly

--- src/interface.ts
+++ src/interface.ts
@@ -2,4 +2,6 @@ export interface Seller {
   id: number;
   name: string;
   address?: string;
+  readonly nib: string; // This only read property
+  readonly npwp: string; // This only read property
 }
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -5,7 +5,23 @@ describe("Interface", () => {
     const seller: Seller = {
       id: 1,
       name: "Toko ABC",
+      nib: "1234534",
+      npwp: "12575389434",
     };
+
+    console.info(seller);
+  });
+
+  it("should support with readonly on properties", () => {
+    const seller: Seller = {
+      id: 1,
+      name: "Toko ABC",
+      nib: "1234534",
+      npwp: "12575389434",
+    };
+
+    seller.name = "Toko Budi";
+    // seller.nib = "324565443"; // Error: Cant update value because it readonly property
 
     console.info(seller);
   });
