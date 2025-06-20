7006e1ad64ab49b54c20a4a3175c3c72d458ca02
Irwanto Siregar <irwanto.regar30@gmail.com>
2025-06-20 15:32:45 +0700

    inheritance on class

---
+++ tests/inheritance.test.ts
@@ -0,0 +1,25 @@
+describe("Class", () => {
+  // parent class
+  class Employee {
+    name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+
+  class Manager extends Employee {}
+
+  class Director extends Manager {}
+
+  it("should can inheritance on class", () => {
+    const employee: Employee = new Employee("Irwan");
+    console.info(employee.name);
+
+    const manager: Manager = new Manager("Budi");
+    console.info(manager.name);
+
+    const director: Director = new Director("Eko");
+    console.info(director.name);
+  });
+});
